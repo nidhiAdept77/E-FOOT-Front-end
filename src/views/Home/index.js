dc1b5652146babbc8ae7cdcf282526a563520852
dhiren <dhiren@aliteprojects.com>
2021-05-22 12:27:39 +0530

    jumbotron and main menu changes

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { FormattedMessage } from 'react-intl'
+import Breadcrumbs from '@components/breadcrumbs'
+import JombotronHome from './components/JombotronHome'
+import GamesComponent from './components/GamesComponent'
+
 
 export default function index() {
     return (
         <div>
-            
+            <Breadcrumbs breadCrumbTitle={<FormattedMessage id="Home" />} breadCrumbActive={<FormattedMessage id="Home" />} />
+            <JombotronHome />
+            <GamesComponent />
         </div>
     )
 }
--- src/views/HowToPlay/components/JombotronHowtoPlay.js
+++ src/views/HowToPlay/components/JombotronHowtoPlay.js
@@ -5,8 +5,8 @@ import { FormattedMessage } from 'react-intl'
 export default function JombotronHowtoPlay() {
     return (
         <Row>
-            <Col className="how-top-play-card">
-                <Jumbotron fluid className="how-to-play-Jumbotron">
+            <Col className="jumbotron-container">
+                <Jumbotron fluid className="jumbotron-div how-to-play-Jumbotron">
                     <Container fluid className="jumbotron-text">
                         <h1 className="display-3"><FormattedMessage id="How To Play" /></h1>
                         <p className="lead"><FormattedMessage id="Play as a Pro not as noob." /></p>
