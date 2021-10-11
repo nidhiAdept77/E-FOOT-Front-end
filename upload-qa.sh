426f2d5cb01731009e5485a0b3dc41f7538df98b
dhiren <dhiren905@gmail.com>
2021-10-11 07:13:10 +0530

    updating envs

--- upload-qa.sh
+++ upload-qa.sh
@@ -1,1 +1,1 @@
-scp -i ~/Desktop/server-keys/dhiren-aws.pem build.tar.gz ubuntu@3.109.175.195:/home/ubuntu/ef-nl-frontend/app/
+scp -i ~/Desktop/server-keys/dhiren-aws.pem build.tar.gz ubuntu@3.109.175.195:/home/ubuntu/efnl-frontend/
