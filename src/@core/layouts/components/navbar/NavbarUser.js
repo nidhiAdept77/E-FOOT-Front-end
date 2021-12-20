cc6d02b0165a1184486eea69860655c33825367f
dhiren <dhiren905@gmail.com>
2021-12-20 10:46:08 +0530

    rank alignment

--- src/@core/layouts/components/navbar/NavbarUser.js
+++ src/@core/layouts/components/navbar/NavbarUser.js
@@ -3,16 +3,16 @@ import IntlDropdown from './IntlDropdown'
 import UserDropdown from './UserDropdown'
 
 // ** Third Party Components
-import { Sun, Moon } from 'react-feather'
+import { Sun, Moon, Star } from 'react-feather'
 import { FaUserSlash, FaUser } from "react-icons/fa"
-import { NavItem, NavLink } from 'reactstrap'
+import { NavItem, NavLink, Badge } from 'reactstrap'
 import { useEffect, useState } from 'react'
 import { CONSTANTS } from '../../../../utils/CONSTANTS'
 import _ from 'underscore'
 
 const NavbarUser = props => {
   // ** Props
-  const { skin, setSkin, showOnlineUserPopup, handleOnlineUserHidden, location } = props
+  const { skin, setSkin, showOnlineUserPopup, handleOnlineUserHidden, location, rank } = props
   const [showOnlinetoggler, setOnlineUserToggler] = useState(true)
   useEffect(() => {
     if (!_.isEmpty(location)) {
@@ -47,6 +47,15 @@ const NavbarUser = props => {
 
   return (
     <ul className='nav navbar-nav align-items-center ml-auto'>
+      <NavItem>
+        <NavLink className='nav-link-style'>
+          {rank && (
+            <Badge  color='light-primary' className="p-1">
+              <span className='text-uppercase'>{rank}</span>
+            </Badge>
+          )}
+        </NavLink>
+      </NavItem>
       <IntlDropdown />
       <NavItem className='d-none d-sm-block d-lg-block'>
         <NavLink className='nav-link-style'>
